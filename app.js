b4e1ef8b2929b076d2f547507d80cefc574a1b15
Aaron <work@aaronsamuel.de>
2021-06-12 21:45:10 +0200

    changed price-plans to bigger font size

--- app.js
+++ app.js
@@ -1,128 +1,128 @@
 // Remove .active class from navbar when in burger menu
-jQuery(document).ready(function($) {
-    var alterClass = function() {
-      var ww = document.body.clientWidth;
-      if (ww < 992) {
-        $('#activeNavbarPagination').removeClass('active').addClass('text-dark');
-      } else if (ww >= 993) {
-        $('#activeNavbarPagination').addClass('active').removeClass('text-dark');
-      };
-    };
-    $(window).resize(function(){
-      alterClass();
-    });
-    //Fire it when the page first loads:
+jQuery(document).ready(function ($) {
+  var alterClass = function () {
+    var ww = document.body.clientWidth;
+    if (ww < 992) {
+      $("#activeNavbarPagination").removeClass("active").addClass("text-dark");
+    } else if (ww >= 993) {
+      $("#activeNavbarPagination").addClass("active").removeClass("text-dark");
+    }
+  };
+  $(window).resize(function () {
     alterClass();
   });
+  //Fire it when the page first loads:
+  alterClass();
+});
 
 // jQuery lightSlider Cover
-$(document).ready(function() {
-    $("#light-slider").lightSlider({
-        item: 3,
-        autoWidth: false,
-        slideMove: 1, // slidemove will be 1 if loop is true
-        slideMargin: 0,
+$(document).ready(function () {
+  $("#light-slider").lightSlider({
+    item: 3,
+    autoWidth: false,
+    slideMove: 1, // slidemove will be 1 if loop is true
+    slideMargin: 0,
 
-        addClass: '',
-        mode: "slide",
-        useCSS: true,
-        cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
-        easing: 'linear', //'for jquery animation',////
+    addClass: "",
+    mode: "slide",
+    useCSS: true,
+    cssEasing: "ease", //'cubic-bezier(0.25, 0, 0.25, 1)',//
+    easing: "linear", //'for jquery animation',////
 
-        speed: 400, //ms'
-        auto: true,
-        pauseOnHover: false,
-        loop: true,
-        slideEndAnimation: true,
-        pause: 8000,
+    speed: 400, //ms'
+    auto: true,
+    pauseOnHover: false,
+    loop: true,
+    slideEndAnimation: true,
+    pause: 8000,
 
-        keyPress: false,
-        controls: true,
-        prevHtml: '',
-        nextHtml: '',
+    keyPress: false,
+    controls: true,
+    prevHtml: "",
+    nextHtml: "",
 
-        rtl:false,
-        adaptiveHeight:false,
+    rtl: false,
+    adaptiveHeight: false,
 
-        vertical:false,
-        verticalHeight:500,
-        vThumbWidth:100,
+    vertical: false,
+    verticalHeight: 500,
+    vThumbWidth: 100,
 
-        thumbItem:10,
-        pager: false,
-        gallery: false,
-        galleryMargin: 5,
-        thumbMargin: 5,
-        currentPagerPosition: 'middle',
+    thumbItem: 10,
+    pager: false,
+    gallery: false,
+    galleryMargin: 5,
+    thumbMargin: 5,
+    currentPagerPosition: "middle",
 
-        enableTouch:true,
-        enableDrag:true,
-        freeMove:true,
-        swipeThreshold: 40,
+    enableTouch: true,
+    enableDrag: true,
+    freeMove: true,
+    swipeThreshold: 40,
 
-        responsive : [],
+    responsive: [],
 
-        onBeforeStart: function (el) {},
-        onSliderLoad: function (el) {},
-        onBeforeSlide: function (el) {},
-        onAfterSlide: function (el) {},
-        onBeforeNextSlide: function (el) {},
-        onBeforePrevSlide: function (el) {}
-    });
+    onBeforeStart: function (el) {},
+    onSliderLoad: function (el) {},
+    onBeforeSlide: function (el) {},
+    onAfterSlide: function (el) {},
+    onBeforeNextSlide: function (el) {},
+    onBeforePrevSlide: function (el) {},
+  });
 });
 
 // jQuery lightSlider Brands
-$(document).ready(function() {
-    $("#light-slider-brands").lightSlider({
-        item: 5,
-        autoWidth: false,
-        slideMove: 1, // slidemove will be 1 if loop is true
-        slideMargin: 0,
+$(document).ready(function () {
+  $("#light-slider-brands").lightSlider({
+    item: 5,
+    autoWidth: false,
+    slideMove: 1, // slidemove will be 1 if loop is true
+    slideMargin: 0,
 
-        addClass: '',
-        mode: "slide",
-        useCSS: true,
-        cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
-        easing: 'linear', //'for jquery animation',////
+    addClass: "",
+    mode: "slide",
+    useCSS: true,
+    cssEasing: "ease", //'cubic-bezier(0.25, 0, 0.25, 1)',//
+    easing: "linear", //'for jquery animation',////
 
-        speed: 400, //ms'
-        auto: true,
-        pauseOnHover: false,
-        loop: true,
-        slideEndAnimation: true,
-        pause: 8000,
+    speed: 400, //ms'
+    auto: true,
+    pauseOnHover: false,
+    loop: true,
+    slideEndAnimation: true,
+    pause: 8000,
 
-        keyPress: false,
-        controls: true,
-        prevHtml: '',
-        nextHtml: '',
+    keyPress: false,
+    controls: true,
+    prevHtml: "",
+    nextHtml: "",
 
-        rtl:false,
-        adaptiveHeight:false,
+    rtl: false,
+    adaptiveHeight: false,
 
-        vertical:false,
-        verticalHeight:500,
-        vThumbWidth:100,
+    vertical: false,
+    verticalHeight: 500,
+    vThumbWidth: 100,
 
-        thumbItem:10,
-        pager: true,
-        gallery: false,
-        galleryMargin: 5,
-        thumbMargin: 5,
-        currentPagerPosition: 'middle',
+    thumbItem: 10,
+    pager: true,
+    gallery: false,
+    galleryMargin: 5,
+    thumbMargin: 5,
+    currentPagerPosition: "middle",
 
-        enableTouch:true,
-        enableDrag:true,
-        freeMove:true,
-        swipeThreshold: 40,
+    enableTouch: true,
+    enableDrag: true,
+    freeMove: true,
+    swipeThreshold: 40,
 
-        responsive : [],
+    responsive: [],
 
-        onBeforeStart: function (el) {},
-        onSliderLoad: function (el) {},
-        onBeforeSlide: function (el) {},
-        onAfterSlide: function (el) {},
-        onBeforeNextSlide: function (el) {},
-        onBeforePrevSlide: function (el) {}
-    });
+    onBeforeStart: function (el) {},
+    onSliderLoad: function (el) {},
+    onBeforeSlide: function (el) {},
+    onAfterSlide: function (el) {},
+    onBeforeNextSlide: function (el) {},
+    onBeforePrevSlide: function (el) {},
+  });
 });

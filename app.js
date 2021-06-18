27408abe000d2b6cb55eac7e894eb7fb0e9f3af0
Aaron <work@aaronsamuel.de>
2021-06-18 12:55:18 +0200

    fixed navigation where the path was wrong + added doppelzimmer page schlafgut

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ jQuery(document).ready(function ($) {
   var alterClass = function () {
     var ww = document.body.clientWidth;
     if (ww < 992) {
-      $(".activeNavbarPagination").removeClass("active").addClass("text-dark border");
+      $(".activeNavbarPagination").removeClass("active").addClass("text-dark shadow-sm border border-2"); //Changing Navigation styles in mobile view
     } else if (ww >= 993) {
-      $(".activeNavbarPagination").addClass("active").removeClass("text-dark border");
+      $(".activeNavbarPagination").addClass("active").removeClass("text-dark shadow-sm border border-2"); //Making it normal again for big view
     }
   };
   $(window).resize(function () {

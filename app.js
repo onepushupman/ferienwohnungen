989a162418eac2188242a9d0223f1d69142805ee
Aaron <work@aaronsamuel.de>
2021-08-23 15:23:32 +0200

    added favicon black, white that changes to machines theme

--- app.js
+++ app.js
@@ -129,3 +129,21 @@ $(document).ready(function () {
     onBeforePrevSlide: function (el) {},
   });
 });
+
+// Changes Favicon based on system settings 
+matcher = window.matchMedia('(prefers-color-scheme: dark)');
+matcher.addListener(onUpdate);
+onUpdate();
+
+lightSchemeIcon = document.querySelector('#light-scheme-icon');
+darkSchemeIcon = document.querySelector('#dark-scheme-icon');
+
+function onUpdate() {
+if (matcher.matches) {
+  lightSchemeIcon.remove();
+  document.head.append(darkSchemeIcon);
+} else {
+  document.head.append(lightSchemeIcon);
+  darkSchemeIcon.remove();
+}
+}

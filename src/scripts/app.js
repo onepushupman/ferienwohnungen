454822cc1caa17cd5ca4d2cca0bd021f60301378
Aaron <work@aaronsamuel.de>
2022-02-20 23:29:27 +0100

    added resize js eventlistener whenever page changes resolution the page refreshes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,5 +2,11 @@ import "./modules/lightSlider";
 import "./modules/burgerMenu";
 import "./modules/favicon";
 
+// Reloads page whenever the resolution changes
+window.addEventListener('resize', function () { 
+    "use strict";
+    window.location.reload(); 
+});
+
 // init safari image fix
 objectFitImages();

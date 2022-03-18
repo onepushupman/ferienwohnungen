ee2091950fcf36bfd84bb07c12576f498187788f
Aaron <work@aaronsamuel.de>
2022-03-18 09:14:39 +0100

    format all the files

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,9 +3,9 @@ import "./modules/burgerMenu";
 import "./modules/favicon";
 
 // Reloads page whenever the resolution changes
-window.addEventListener('resize', function () { 
-    "use strict";
-    window.location.reload(); 
+window.addEventListener("resize", function () {
+  "use strict";
+  window.location.reload();
 });
 
 // init safari image fix
